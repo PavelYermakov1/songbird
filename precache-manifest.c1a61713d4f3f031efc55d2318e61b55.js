bde096f58d40f13c609c2bb93fc589be7d2a5cf0
Pavel Yermakov <pavelak137@mail.ru>
2020-09-01 20:44:24 +0300

    Updates

---
+++ precache-manifest.c1a61713d4f3f031efc55d2318e61b55.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160786e8e2ddf72d526d1fd7f2426328",
+    "url": "/songbird/index.html"
+  },
+  {
+    "revision": "fddccbd13018430e100d",
+    "url": "/songbird/static/css/2.e3f36e05.chunk.css"
+  },
+  {
+    "revision": "060ed0a10ca9fa891342",
+    "url": "/songbird/static/css/main.21b810cb.chunk.css"
+  },
+  {
+    "revision": "fddccbd13018430e100d",
+    "url": "/songbird/static/js/2.e756a2a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/songbird/static/js/2.e756a2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060ed0a10ca9fa891342",
+    "url": "/songbird/static/js/main.f07680d7.chunk.js"
+  },
+  {
+    "revision": "41fc37381d3a97757444",
+    "url": "/songbird/static/js/runtime-main.710c5ffc.js"
+  },
+  {
+    "revision": "8956b93817686881283802c65322f568",
+    "url": "/songbird/static/media/congratulation.8956b938.mp3"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/songbird/static/media/fake-bird.06a46938.jpg"
+  },
+  {
+    "revision": "8d554ba1d3f5394ae41e9cd43effa3ee",
+    "url": "/songbird/static/media/logo.8d554ba1.png"
+  },
+  {
+    "revision": "769c9f2ee3ed0e8bd282f7f454ac0215",
+    "url": "/songbird/static/media/pause.769c9f2e.svg"
+  },
+  {
+    "revision": "25582e1c0bc533adfd5e6411863226fe",
+    "url": "/songbird/static/media/play.25582e1c.svg"
+  },
+  {
+    "revision": "a1e9e8b671592af8b3a503b23bbba6eb",
+    "url": "/songbird/static/media/right.a1e9e8b6.mp3"
+  },
+  {
+    "revision": "165166d5c9e027367cb75b2ac89dcfc0",
+    "url": "/songbird/static/media/wrong.165166d5.mp3"
+  }
+]);
